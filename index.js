31c16b1e4255466cf4e9e6e8bc6ed13925c5c3c5
godwinolekanma <godwinolekanma12@gmail.com>
2024-05-23 11:58:39 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+       buttonInner = this.innerHTML;
+       makesound(buttonInner)
+       buttonFlash(buttonInner)
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    makesound(event.key);
+    buttonFlash(event.key);
+});
+
+function makesound(key){
+    switch (key){
+        case "w":
+            var audio = new Audio("sounds/crash.mp3")
+            audio.play();
+
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3")
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3")
+            audio.play();
+            break;  
+
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3")
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3")
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3")
+            audio.play();
+            break;
+        
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3")
+            audio.play();
+            break;
+        
+        default: console.log(buttonInner);
+       }
+}
+
+function buttonFlash(key){
+    activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    }, 100)
+}
